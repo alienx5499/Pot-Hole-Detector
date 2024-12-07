36e40e31d01840c4ddb91b69fbfd53da1da28813
jaisheel12 <jaisheel021@gmail.com>
2024-12-07 13:14:11 +0530

    Added Camera and map functionality

---
+++ Pot-Hole-Detector/app/(tabs)/_layout.tsx
@@ -0,0 +1,9 @@
+import { Stack, Tabs } from "expo-router";
+import React from "react";
+
+export default function Layout() {
+  return <Tabs >
+    <Tabs.Screen name='index' options={{ headerShown: false }} />
+    <Tabs.Screen name='camera' options={{ headerShown: false }} />
+  </Tabs>;
+}
--- Pot-Hole-Detector/app/_layout.tsx
+++ Pot-Hole-Detector/app/_layout.tsx
@@ -1,5 +1,9 @@
 import { Stack } from "expo-router";
+import React from "react";
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack >
+    <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    <Stack.Screen name="maps" options={{ headerShown: false  }} />  
+  </Stack>;
 }
